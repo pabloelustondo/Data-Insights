846f84a027d21ab8c14a41b44f8bb9a0bb75c0db
pabloelustondo <pablo.elustondo@gmail.com>
2016-11-25 10:34:30 -0500

    CB-89 - Cosmetic Changes to send parameters to ODA

--- dad/app/chart.component.js
+++ dad/app/chart.component.js
@@ -23,7 +23,41 @@ var DadChartComponent = (function () {
     function DadChartComponent(dadChartDataService) {
         this.dadChartDataService = dadChartDataService;
     }
+    DadChartComponent.prototype.drawChartBar = function (chartConfig, data) {
+        if (!data)
+            return;
+        var testdata = [];
+        for (var _i = 0, _a = data.result; _i < _a.length; _i++) {
+            var r = _a[_i];
+            testdata.push({ "label": r.Rng, "value": r.NumberOfDevices });
+        }
+        var historicalBarChart = [
+            {
+                key: "Cumulative Return",
+                values: testdata }];
+        var width = 300;
+        var height = 300;
+        nv.addGraph(function () {
+            var chart = nv.models.discreteBarChart()
+                .x(function (d) { return d.label; })
+                .y(function (d) { return d.value; })
+                .staggerLabels(true)
+                .showValues(true)
+                .duration(250);
+            d3.select("#" + chartConfig.id)
+                .datum(historicalBarChart)
+                .call(chart);
+            nv.utils.windowResize(chart.update);
+            return chart;
+        });
+    };
     DadChartComponent.prototype.drawChart = function (chartConfig, data) {
+        if (chartConfig.type === 'pie')
+            this.drawChartPie(chartConfig, data);
+        if (chartConfig.type === 'bar')
+            this.drawChartBar(chartConfig, data);
+    };
+    DadChartComponent.prototype.drawChartPie = function (chartConfig, data) {
         if (!data)
             return;
         var testdata = [];
--- dad/app/mock.charts.ts
+++ dad/app/mock.charts.ts
@@ -4,21 +4,54 @@
 import { DadChart } from './chart.component';
 
 export const CHARTS: DadChart[] = [
-    {   id: "chart1", name: 'Number of Devices by Range Pie Chart 1',
+    {   id: "chart1",
+        name: 'Number of Devices by Range Pie Chart 1',
+        type: "pie",
         parameters: [
             {   parameterType:"DateRange",
                 dateFrom: "2016-08-16",
                 dateTo: "2016-08-18"
             }]
     },
-    { id: "chart2", name: 'Number of Devices by Range Pie Chart 2',
+    {   id: "chart2",
+        name: 'Number of Devices by Range Pie Chart 2',
+        type: "pie",
         parameters: [
             {   parameterType:"DateRange",
                 dateFrom: "2016-08-19",
                 dateTo: "2016-08-22"
             }]
     },
-    { id: "chart3", name: 'Number of Devices by Range Pie Chart 3',
+    {   id: "chart3",
+        name: 'Number of Devices by Range Pie Chart 3',
+        type: "pie",
+        parameters: [
+            {   parameterType:"DateRange",
+                dateFrom: "2016-08-23",
+                dateTo: "2016-08-25"
+            }]
+    },
+    {   id: "chart4",
+        name: 'Number of Devices by Range Pie Chart 1',
+        type: "bar",
+        parameters: [
+            {   parameterType:"DateRange",
+                dateFrom: "2016-08-16",
+                dateTo: "2016-08-18"
+            }]
+    },
+    {   id: "chart5",
+        name: 'Number of Devices by Range Pie Chart 2',
+        type: "bar",
+        parameters: [
+            {   parameterType:"DateRange",
+                dateFrom: "2016-08-19",
+                dateTo: "2016-08-22"
+            }]
+    },
+    {   id: "chart6",
+        name: 'Number of Devices by Range Pie Chart 3',
+        type: "bar",
         parameters: [
             {   parameterType:"DateRange",
                 dateFrom: "2016-08-23",
