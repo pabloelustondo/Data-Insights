a9c1e6dca745b53c1583e98c11b0fa77b733bb2c
Varun Dave <varun.dave@soti.net>
2016-12-08 13:45:38 -0500

    CB-112:add model and update controller to pass data to aws

--- ida/src/controllers/usersController.ts
+++ ida/src/controllers/usersController.ts
@@ -1,7 +1,14 @@
 import {Route, Get, Post, Delete, Patch, Example} from 'tsoa';
 import {SDS} from '../models/user';
+import {SDSBattery} from '../models/batteryData';
+
+import * as express from 'express';
+import * as methodOverride from 'method-override';
+
 // import * as https from 'https';
 const config = require('../../appconfig.json');
+const awsPush = require('../../../pdic/src/awsPush');
+
 import * as querystring from 'querystring';
 import * as rp from 'request-promise';
 @Route('Data')
@@ -11,15 +18,16 @@ export class SDSController {
      * Post a unit of data to be stored in the cloud analytics database
      */
 
+
     @Post()
     @Example<any>({
     metadata: 'here is where metadata explaining the data should go',
     createdAt: '2016-08-08',
     data: ['aaa', 'bbb', 'ccc']
     })
-    public async Create(request: SDS, optionalString?: string): Promise<SDS> {
+    public async Create(request: SDSBattery, optionalString?: string): Promise<SDS> {
 
-        this.amazonCall(request, optionalString);
+        awsPush.putRecord(request);
         return  {
             metadata: 'Thanks a lot',
             createdAt: new Date()
@@ -27,11 +35,15 @@ export class SDSController {
     }
 
     private async amazonCall (request: SDS, optionString?: string): Promise<SDS> {
+
+
         return  {
             metadata: 'Thanks a lot',
             createdAt: new Date()
         };
     }
+
+
     // Build the post string from an object
 
 
---
+++ ida/src/models/batteryData.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by vdave on 12/8/2016.
+ */
+export interface SDSBattery {
+    dev_id: string;
+    server_time_stamp:  Date;
+    int_value: number;
+    stat_type: number;
+    time_stamp: Date;
+
+}
