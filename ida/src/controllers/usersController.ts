d6c8a10660cbb83b8590f3b322848de0b1c60708
Varun Dave <varun.dave@soti.net>
2016-12-09 11:14:17 -0500

    CB-112: Move standalone awsPush from PDIC to ida. Update controller to use local information. Update pakcahges within ida to use aws-sdk. Update appcongif with keys which will be used by modules withing ida.

--- ida/src/controllers/usersController.ts
+++ ida/src/controllers/usersController.ts
@@ -7,7 +7,8 @@ import * as methodOverride from 'method-override';
 
 // import * as https from 'https';
 const config = require('../../appconfig.json');
-const awsPush = require('../../../pdic/src/awsPush');
+const awsPush = require('../awsPush');
+
 
 import * as querystring from 'querystring';
 import * as rp from 'request-promise';
@@ -15,17 +16,28 @@ import * as rp from 'request-promise';
 export class SDSController {
 
     /**
-     * Post a unit of data to be stored in the cloud analytics database
+     * Post a unit of data to be stored in the cloud analytics database.
+     * This api allows a user to post device data.
      */
 
 
     @Post()
     @Example<any>({
-    metadata: 'here is where metadata explaining the data should go',
-    createdAt: '2016-08-08',
-    data: ['aaa', 'bbb', 'ccc']
+        headers: {
+            'X-API-key': 'Future Private Access Key',
+            'Accept': 'application/json'
+        },
+        json: true,
+        url: 'http://localhost:3003/data',
+        data: {
+            dev_id: '12345678901234567890123456789012345678901234567890123456789012345678901234567890',
+            server_time_stamp: '2016-12-08T19:12:15.235Z',
+            int_value: 123456789123,
+            stat_type: 1234,
+            time_stamp: '2016-12-08T19:13:15.235Z'
+        }
     })
-    public async Create(request: SDSBattery, optionalString?: string): Promise<SDS> {
+    public async Create(request: SDSBattery): Promise<SDS> {
 
         awsPush.putRecord(request);
         return  {
@@ -34,18 +46,5 @@ export class SDSController {
         };
     }
 
-    private async amazonCall (request: SDS, optionString?: string): Promise<SDS> {
-
-
-        return  {
-            metadata: 'Thanks a lot',
-            createdAt: new Date()
-        };
-    }
-
-
     // Build the post string from an object
-
-
-
 }
