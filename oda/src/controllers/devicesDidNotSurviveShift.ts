bbefd4a48b1bc681907ed61786fc4d3805478235
Varun Dave <varun.dave@soti.net>
2016-12-15 13:23:25 -0500

    CB:142 - Update the description and expected result for parameters

--- oda/src/controllers/devicesDidNotSurviveShift.ts
+++ oda/src/controllers/devicesDidNotSurviveShift.ts
@@ -11,80 +11,43 @@ import * as rp from 'request-promise';
 @Route('Devices')
 export class CountDevicesNotSurvivedShiftController {
     /**
-     * A DischargeRate represents how quickly the battery is losing the charge per hour.
-     * This data is retrieved every hour for a given day.
-     * A date range is required to get the information.
+     * Number of devices that did not last the full shift for a given date, shift time, and shift duration. \n
+     * If no dateAndShift field is provided, the default date of previous day with a shift starting at 8:00 am \n
+     * is used as data and shift time. \n
+     *
+     *     The duration is a number representing the length of the shift in hours.
+     *     Eg. A shift of 8 hours can be represented as 8, 8.0
+     *     A shift of 7.5 hours can be represented as 7.5
+     *
+     *     The dataAndShift field must be in date format YYYY-MM-DDTHH:MM:SS
+     *     where YYYY-MM-DD = Year in 4 digits, followed by month, followed by day of the month
+     *     T - A static string value
+     *     HH:MM:SS - Hour, minute and seconds.
+     *
      */
 
     @Get('Battery/Summary/DevicesNotSurvivedShift')
     @Example<any>({
         'createdAt': '2016-11-29T20:30:21.385Z',
+        'metadata': [
+            'CountDevicesNotLastedShift: Count of devices that did not last full shift',
+            'TotalActiveDevices: Total devices active per day'
+        ],
         'data': [
             {
-                'NumberOfDevices': '4',
-                'DischargeRate': 2
-            },
-            {
-                'NumberOfDevices': '6',
-                'DischargeRate': 3
-            },
-            {
-                'NumberOfDevices': '24',
-                'DischargeRate': 4
-            },
-            {
-                'NumberOfDevices': '58',
-                'DischargeRate': 5
-            },
-            {
-                'NumberOfDevices': '53',
-                'DischargeRate': 6
-            },
-            {
-                'NumberOfDevices': '27',
-                'DischargeRate': 7
-            },
-            {
-                'NumberOfDevices': '18',
-                'DischargeRate': 8
-            },
-            {
-                'NumberOfDevices': '3',
-                'DischargeRate': 9
-            },
-            {
-                'NumberOfDevices': '3',
-                'DischargeRate': 10
-            },
-            {
-                'NumberOfDevices': '1',
-                'DischargeRate': 12
-            },
-            {
-                'NumberOfDevices': '1',
-                'DischargeRate': 14
-            },
-            {
-                'NumberOfDevices': '4',
-                'Percentage': 16
-            },
-            {
-                'NumberOfDevices': '14',
-                'DischargeRate': 17
-            },
-            {
-                'NumberOfDevices': '1',
-                'DischargeRate': 22
-            },
-            {
-                'NumberOfDevices': '1',
-                'DischargeRate': 45
+                'CountDevicesNotLastedShift': 25,
+                'TotalActiveDevices': 100
             }
         ]
     })
-    public async Get(startTime: string, duration: string, date: string): Promise<SDS> {
+    public async Get(duration: number, dateAndShift?: Date): Promise<SDS> {
 
-        const xqs = {startTime: startTime, duration: duration, date : date};
+        if (!dateAndShift) {
+            console.log( 'no date and time provided' );
+
+        }
+
+        const xqs = {duration: duration, date : dateAndShift};
         const xurl = 'https://' + config['aws-hostname'] + config['aws-deviceNotLasted'];
 
         const options: rp.OptionsWithUrl = {
@@ -98,7 +61,7 @@ export class CountDevicesNotSurvivedShiftController {
         };
 
         let p = await rp(options); // request library used
-        let mData = ['CountDevicesNotLastedShift :Count of devices that did not last full shift', 'TotalActiveDevices: Total devices active per day'];
+        let mData = ['CountDevicesNotLastedShift: Count of devices that did not last full shift', 'TotalActiveDevices: Total devices active per day'];
 
         const user: SDS = {
             createdAt: new Date(),
