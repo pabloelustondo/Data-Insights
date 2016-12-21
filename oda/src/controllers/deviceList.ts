c2533a18c1e4b74e422d8e0fde1648bda407e79f
Varun Dave <varun.dave@soti.net>
2016-12-21 12:21:07 -0500

    CB-155: update documentation and values returned for listOfDevices api

--- oda/src/controllers/deviceList.ts
+++ oda/src/controllers/deviceList.ts
@@ -8,25 +8,76 @@ const config = require('../../appconfig.json');
 
 @Route('Devices')
 export class ListOfDevices {
-    @Post('/getList')
-    @Example<any>({
-        headers: {
-            'X-API-key': 'Future Private Access Key',
-            'Accept': 'application/json'
-        },
-        json: true,
-        url: 'http://localhost:3002/devices/getList',
-        data: {
-            deviceId: '1234avcde',
-            deviceProperties: ['deviceName', 'deviceManufacturer', 'deviceOwner', 'deviceDriver']
-        }
+    @Post('getList')
+    @Example<DeviceModel>({
+       deviceId: '1234avcde',
+       deviceProperties: ['deviceName', 'deviceManufacturer', 'deviceOwner', 'deviceDriver']
     })
 
     public async Create(request: DeviceModel): Promise<DeviceModel> {
+
+        let requestProperties = request.deviceProperties;
+
+        let responseProperties: string[] = [];
+
+        let date = new Date();
+        for (let prop in requestProperties) {
+            let respP = this.getValueOfProperty(requestProperties[prop], date);
+            responseProperties.push(requestProperties[prop] + ' : ' + respP);
+        }
+
         return{
-            deviceId: '1234avcde',
-            deviceProperties: ['deviceName', 'deviceManufacturer', 'deviceOwner', 'deviceDriver']
+            deviceId: request.deviceId,
+            deviceProperties: responseProperties
         };
+    }
+
+    public getValueOfProperty(propName: string, date: Date) {
+
+        let ms = date.getMilliseconds();
+
+        if (propName === 'deviceName') {
+            if (ms % 3 === 0) {
+                return 'S7';
+            } else if (ms % 3 === 1) {
+                return 'ipad';
+            } else {
+                return 'windows ce m123344';
+            }
+        } else if (propName === 'deviceManufacturer') {
+            if (ms % 3 === 0) {
+                return 'Samsung';
+            } else if (ms % 3 === 1) {
+                return 'Apple';
+            } else {
+                return 'Microsoft';
+            }
+        } else if (propName === 'deviceDriver') {
+            if (ms % 3 === 0) {
+                return 'Pablo';
+            } else if (ms % 3 === 1) {
+                return 'Doga';
+            } else {
+                return 'Tim';
+            }
+        }else if (propName === 'deviceOwner') {
+            if (ms % 3 === 0) {
+                return 'Varun';
+            } else if (ms % 3 === 1) {
+                return 'Nathan';
+            } else {
+                return 'Sergey';
+            }
+        } else {
+            if (ms % 3 === 0) {
+                return 'undefined';
+            } else if (ms % 3 === 1) {
+                return 'undefined';
+            } else {
+                return 'undefined';
+            }
+
+        }
     }
 }
 
