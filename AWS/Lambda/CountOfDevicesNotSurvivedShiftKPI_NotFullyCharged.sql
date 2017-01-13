89b6820b19214d23481b0768add479c016831e7d
Sergey Khanzhin <Sergey.Khanzhin@soti.net>
2017-01-13 15:13:29 -0500

    CB:184 initial query check in

---
+++ AWS/Lambda/CountOfDevicesNotSurvivedShiftKPI_NotFullyCharged.sql
@@ -0,0 +1,71 @@
+CREATE temporary TABLE tbl1
+                (
+                devid VARCHAR(80) NOT NULL ENCODE lzo DISTKEY,
+                time_stamp timestamp,
+                occ INTEGER NOT NULL ENCODE delta,
+                intvalue int
+                )
+            SORTKEY
+                (
+                devid,
+                time_stamp
+                );
+        insert into tbl1 
+        (
+            with variables as (    
+                SELECT
+                '$[shiftStartDateTime]'::timestamp as shiftStartTime
+                )
+            select 
+				devid, 
+				time_stamp, 
+				case 
+    				when intvalue-lead(intvalue, 1) over (partition by devid order by time_stamp)<0 
+            			OR (intvalue < 10) 
+					then 1 -- device got charged
+    				else 0 -- device is discharging or on charger
+				end occ, 
+				intvalue
+			from devstatint d, variables
+			where time_stamp between shiftStartTime and dateadd(hour, $[shiftDuration], shiftStartTime)
+                and stattype=-1)
+;
+select devid into #t
+from tbl1
+group by devid
+having sum(occ)<>0;
+
+SELECT 
+count(*) as count_Device_Not_Fully_Charged
+FROM 
+(
+	with variables as (    
+		                SELECT
+		                '$[shiftStartDateTime]'::timestamp as shiftStartTime
+		                )
+	SELECT                      
+        d.DevId,
+        intvalue AS cur_value,                               
+		LEAD(intvalue,1) OVER (PARTITION BY d.devid ORDER BY time_stamp) next_value,                               
+        LEAD(intvalue,2) OVER (PARTITION BY d.devid ORDER BY time_stamp) next2_value,                               
+        time_stamp,                               
+        LEAD(time_stamp,1) OVER (PARTITION BY d.devid ORDER BY time_stamp) next_time,                               
+        LEAD(time_stamp,2) OVER (PARTITION BY d.devid ORDER BY time_stamp) next2_time,
+        row_number() over (PARTITION BY d.devid ORDER BY time_stamp) rownum							
+        FROM devstatint d, variables, #t t
+	where t.DevId = d.DevId
+		 and  time_stamp between shiftStartTime and dateadd(hour, $[shiftDuration], shiftStartTime)
+		 and StatType = -1
+	)t
+WHERE 
+(
+		next_value < 100 
+	and next2_value < 100
+	and next2_value < next_value
+	and cur_value=100
+	and (cur_value - next_value)*60 / NULLIF(DATEDIFF(MINUTE,time_stamp,next_time),0) > 10
+	and (next_value - next2_value)*60 / NULLIF(DATEDIFF(MINUTE,next_time, next2_time),0) < 10	
+)
+or 
+(rownum=1 and cur_value<100)
+;
