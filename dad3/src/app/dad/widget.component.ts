544632e2b1a3783d6270a6a55f9d1533b968d217
Doga Ister <doga.ister@soti.net>
2017-01-24 16:25:10 -0500

    CB203: widget is refactored but refresh doesn't work and it is in progress

--- dad3/src/app/dad/widget.component.ts
+++ dad3/src/app/dad/widget.component.ts
@@ -64,55 +64,7 @@ export class DadWidget extends DadElement{
                     </div><br/><br/><br>
                     </div>  
                     
-                    
-                  <div class="row">
-                      <div *ngIf="editMode">          
-                        <div *ngFor="let uiparam of widget.uiparameters">
-                           <div><label>{{uiparam.Name}}</label></div>
-                           <div *ngIf="uiparam.Type == dadParameterType.DateTime">
-                           <input type="date" [(ngModel)]="uiparam.Value['D']"/>       
-                           <timepicker [(ngModel)]="uiparam.Value['T']" (change)="changed()" [hourStep]="hstep" [minuteStep]="mstep" [showMeridian]=false [readonlyInput]="false"></timepicker>       
-                           </div>
-            
-                           <div *ngIf="uiparam.Type == dadParameterType.Duration">
-                           <timepicker [(ngModel)]="uiparam.Value" (change)="changed()" [hourStep]="hstep" [minuteStep]="mstep" [showMeridian]=false [readonlyInput]="false"></timepicker>
-                           </div>
-                           <div *ngIf="uiparam.Type == dadParameterType.Number"><input type="number" min="0" max="100" [(ngModel)]="uiparam.Value" /></div>  
-                           <div *ngIf="uiparam.Type == dadParameterType.String"><input type="text" [(ngModel)]="uiparam.Value" /></div>   
-                 
-                        </div>
-                        <!--refresh button here-->
-                        <br/>
-                        <div class="col-md-4 text-center">
-                        <button (click)="onRefresh()" style="border-color:white; color:white; margin-left:-15px;" type="button" class="btn btn-outline-primary">Refresh</button>
-                        <br/><br/>
-                        </div>
-                        <div>
-                        <button (click)="onEdit()" style="color:white;" type="button" class="btn btn-outline-primary pull-right">
-                            <span class="glyphicons glyphicons-remove"></span>
-                        </button>
-                        </div>     
-                      </div>
-                  </div>
-                  
-                  <div class="chart-wrapper px-1" style="height:70px;">
-                      <canvas baseChart class="chart" [datasets]="lineChart1Data" [labels]="lineChart1Labels" [options]="lineChart1Options" [colors]="lineChart1Colours" [legend]="lineChart1Legend" [chartType]="lineChart1Type" (chartHover)="chartHovered($event)" (chartClick)="chartClicked($event)"></canvas>
-                  </div>
- 
-                  <div class="row">
-                      <div *ngIf="!editMode">          
-                        <span *ngFor="let uiparam of widget.uiparameters">
-                   <!--  <div><label style="text-decoration: underline">{{uiparam.Name}} :</label></div> -->
-                           <span *ngIf="uiparam.Type == dadParameterType.DateTime">
-                           {{uiparam.Value['D']  }}                        
-                           </span>
-                   <!--        <div *ngIf="uiparam.Type == dadParameterType.Duration">{{addingZero(uiparam.Value.getHours())}}:{{addingZero(uiparam.Value.getMinutes())}}</div>
-                           <div *ngIf="uiparam.Type == dadParameterType.Number">{{uiparam.Value}}</div>   -->
-                            <span *ngIf="uiparam.Type == dadParameterType.String && uiparam.Value!='custom'">({{uiparam.Value}})</span> 
-                        </span>    
-                      </div>
-                  </div>
-                 
+                    <dadparameters [element]="widget" [editMode]="editMode"></dadparameters>  
                 </div>       
      </div>
   </div>     
@@ -132,7 +84,6 @@ export class DadWidgetComponent implements OnInit {
               private dadWidgetConfigsService: DadWidgetConfigsService) {}
 
   onRefresh(message:string):void{
-    this.mapParameters2model();
     this.dadWidgetConfigsService.saveOne(this.widget);
     this.dadWidgetDataService.getElementData(this.widget).then(
         data => {
@@ -166,87 +117,6 @@ export class DadWidgetComponent implements OnInit {
     );
   }
 
-    mapParameters2model():void{
-        //this action will map UI parameters into model parameters back
-        let parameters = this.widget.parameters[0];   //maybe we need to stop having a list?
-        for (let uiparam of this.widget.uiparameters) {
-            if (uiparam.Type === this.dadParameterType.DateTime) {
-
-                let datetime:Date = new Date(uiparam.Value['D']);
-                let time:Date = uiparam.Value['T'];
-                datetime.setUTCHours(time.getUTCHours(), time.getUTCMinutes());
-                parameters[uiparam.DataSource] = datetime.toISOString();
-
-            }
-            if (uiparam.Type === this.dadParameterType.Number) {
-                parameters[uiparam.DataSource] = uiparam.Value;
-            }
-            if (uiparam.Type === this.dadParameterType.String) {
-                parameters[uiparam.DataSource] = uiparam.Value;
-            }
-            if (uiparam.Type === this.dadParameterType.Duration) {
-                parameters[uiparam.DataSource] = this.mapDate2LongDuration(uiparam.Value);
-            }
-        }
-
-    }
-
-    mapParameters2ui():void{
-        //this action will map model parameters into UI parameters
-        let parameters = this.widget.parameters[0];   //maybe we need to stop having a list?
-        for (let uiparam of this.widget.uiparameters) {
-            if (uiparam.Type === this.dadParameterType.DateTime) {
-                let d: Date;
-                if (parameters[uiparam.DataSource+"Auto"]=="yesterday"){
-                     let dold = new Date(parameters[uiparam.DataSource]);
-                     let hrs = dold.getHours();
-                     let mins = dold.getMinutes();
-                     let secs  = dold.getSeconds();
-                     d = new Date();
-                     d.setDate(d.getDate() - 1);
-                     d.setHours(hrs,mins,secs);
-                }else{ // we assume that we have a valid date
-                     d = new Date(parameters[uiparam.DataSource]);
-                }
-                let yyyy = d.getFullYear();
-                let m = d.getMonth()+1;
-                let day = d.getDate();
-                let mm = (m <10 )? "0" + m : "" + m;
-                let dd = (day <10 )? "0" + day : "" + day;
-                uiparam.Value = {};
-                uiparam.Value['D'] = yyyy + "-" + mm + "-" + dd;
-                uiparam.Value['T'] = d;
-            }
-            if (uiparam.Type === this.dadParameterType.Number) {
-                uiparam.Value = parameters[uiparam.DataSource];
-            }
-            if (uiparam.Type === this.dadParameterType.String) {
-                uiparam.Value = parameters[uiparam.DataSource];
-            }
-            if (uiparam.Type === this.dadParameterType.Duration) {
-                let Iduration: number = parameters[uiparam.DataSource];
-                let Tduration = this.mapLongDuration2Date(Iduration);
-                uiparam.Value = Tduration;
-            }
-        }
-    }
-
-    mapLongDuration2Date ( duration:number): Date {
-
-      let hrs = Math.floor(duration);
-      let mins = (duration - hrs) * 60;
-      let time = new Date();
-      time.setHours(hrs,mins);
-      return time;
-    }
-
-    mapDate2LongDuration ( duration:Date): number {
-
-        let hrs = duration.getHours();
-        let mins = duration.getMinutes();
-        let durationLong:number = hrs + mins/60;
-        return durationLong;
-    }
 
     percentageOfTotal(){
       if(this.data[this.widget.metrics[0].DataSource] == 0){
@@ -266,8 +136,6 @@ export class DadWidgetComponent implements OnInit {
 
   ngOnInit() {
     console.log("Widgets are loading... :" + this.widget.id);
-    this.mapParameters2ui();
-    this.mapParameters2model();
      // this.mapParameters2ui();
     this.dadWidgetDataService.getElementData(this.widget).then(
       data => {
