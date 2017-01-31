5572d2f4f791f713d6ccb4acfe1614eaff31fc65
Varun Dave <varun.dave@soti.net>
2017-01-31 13:55:04 -0500

    CB-215: fix dad and oda to allowe x-header-token

--- oda/src/server.ts
+++ oda/src/server.ts
@@ -46,7 +46,8 @@ app.use(methodOverride());
 
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+
     next();
 });
 
