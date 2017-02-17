0fd6c0175038842fa4a9dd9e0e1011afe99f1cfa
nathan373 <nmalink@gmail.com>
2017-02-17 11:25:13 -0500

    CB-260 Create a data flow on MCDP to transfer DeviceStatApplication

--- Database/MCDA/Procedures/MCDA.DeviceStatApplication_GetAll.sql
+++ Database/MCDA/Procedures/MCDA.DeviceStatApplication_GetAll.sql
@@ -31,9 +31,9 @@ BEGIN
 			BEGIN
 				Select @lastTimeTmp = max(a.ts) 
 				from (
-					select top (@BatchSize) [timestamp] as ts from [$(MobiControlDB)].dbo.DeviceStatApplication WITH (NOLOCK) 
-					where [timestamp] > @lastTime
-					order by [timestamp] asc
+					select top (@BatchSize) Endtime as ts from [$(MobiControlDB)].dbo.DeviceStatApplication WITH (NOLOCK) 
+					where Endtime > @lastTime
+					order by Endtime asc
 					 ) a
 
 				IF( @lastTimeTmp IS NOT NULL)
@@ -41,10 +41,10 @@ BEGIN
 					EXEC [MCDA].[DeviceSyncStatus_Update] @Name=@tablename, @Status=0, @LastSyncTime=@lastTimeTmp, @PreviousSyncTime=@lastTime
 				END
 
-				SELECT d.DevId as DeviceId, A.[TimeStamp], A.[StatType], A.[IntValue], A.[ServerDateTime]
+				SELECT A.DevId, A.AppId, A.StartTime, A.EndTime, DATEADD(HOUR, DATEDIFF(HOUR, 0, StartTime), 0) AS StartTimeRounded, DATEADD(HOUR, DATEDIFF(HOUR, 0, EndTime), 0) AS EndTimeRounded
 					FROM [$(MobiControlDB)].dbo.DeviceStatApplication AS A WITH (NOLOCK) 
-					INNER JOIN [$(MobiControlDB)].dbo.devInfo as D WITH (NOLOCK) ON A.DeviceId = D.DeviceId
-					WHERE [timestamp] > @lastTime and [timestamp] <= @lastTimeTmp
+					INNER JOIN [$(MobiControlDB)].dbo.devInfo as D WITH (NOLOCK) ON A.DevId = D.DevId
+					WHERE Endtime > @lastTime and Endtime <= @lastTimeTmp
 			END
 		ELSE					--failed
 			BEGIN
@@ -52,19 +52,19 @@ BEGIN
 				BEGIN
 					EXEC [MCDA].[DeviceSyncStatus_Update] @Name=@tablename, @Status=1
 
-					SELECT d.DevId as DeviceId, A.[TimeStamp], A.[StatType], A.[IntValue], A.[ServerDateTime]
+					SELECT A.DevId, A.AppId, A.StartTime, A.EndTime, DATEADD(HOUR, DATEDIFF(HOUR, 0, StartTime), 0) AS StartTimeRounded, DATEADD(HOUR, DATEDIFF(HOUR, 0, EndTime), 0) AS EndTimeRounded
 					FROM [$(MobiControlDB)].dbo.DeviceStatApplication AS A WITH (NOLOCK) 
-					INNER JOIN [$(MobiControlDB)].dbo.devInfo as D WITH (NOLOCK) ON A.DeviceId = D.DeviceId
-					WHERE [timestamp] > @PreviousTime and [timestamp] <= @lastTime
+					INNER JOIN [$(MobiControlDB)].dbo.devInfo as D WITH (NOLOCK) ON A.DevId = D.DevId
+					WHERE Endtime > @PreviousTime and Endtime <= @lastTime
 				END
 				ELSE
 				BEGIN
 					EXEC [MCDA].[DeviceSyncStatus_Update] @Name=@tablename, @Status=1
 
-					SELECT d.DevId as DeviceId, A.[TimeStamp], A.[StatType], A.[IntValue], A.[ServerDateTime]
+					SELECT A.DevId, A.AppId, A.StartTime, A.EndTime, DATEADD(HOUR, DATEDIFF(HOUR, 0, StartTime), 0) AS StartTimeRounded, DATEADD(HOUR, DATEDIFF(HOUR, 0, EndTime), 0) AS EndTimeRounded
 					FROM [$(MobiControlDB)].dbo.DeviceStatApplication AS A WITH (NOLOCK) 
-					INNER JOIN [$(MobiControlDB)].dbo.devInfo as D WITH (NOLOCK) ON A.DeviceId = D.DeviceId
-					WHERE A.[timestamp] <= @lastTime
+					INNER JOIN [$(MobiControlDB)].dbo.devInfo as D WITH (NOLOCK) ON A.DevId = D.DevId
+					WHERE A.Endtime <= @lastTime
 				END
 				
 			END
@@ -72,17 +72,17 @@ BEGIN
 	ELSE 
 	BEGIN
 		Select @lastTime = max(a.ts) from (
-			select top (@BatchSize) [timestamp] as ts from [$(MobiControlDB)].dbo.DeviceStatApplication WITH (NOLOCK) order by [timestamp] asc
+			select top (@BatchSize) Endtime as ts from [$(MobiControlDB)].dbo.DeviceStatApplication WITH (NOLOCK) order by Endtime asc
 			) a
 
 		IF (@lastTime IS NOT NULL AND NOT EXISTS(SELECT 1 from [MCDA].[DeviceSyncStatus] where Name = @tablename))
 		BEGIN
 			EXEC [MCDA].[DeviceSyncStatus_Insert] @Name = @tablename, @status = 1, @LastSyncTime = @lastTime
 
-			SELECT d.DevId as DeviceId, A.[TimeStamp], A.[StatType], A.[IntValue], A.[ServerDateTime]
-				FROM [$(MobiControlDB)].dbo.DeviceStatApplication AS A WITH (NOLOCK) 
-				INNER JOIN [$(MobiControlDB)].dbo.devInfo as D WITH (NOLOCK) ON A.DeviceId = D.DeviceId
-				WHERE A.[timestamp] <= @lastTime
+			SELECT A.DevId, A.AppId, A.StartTime, A.EndTime, DATEADD(HOUR, DATEDIFF(HOUR, 0, StartTime), 0) AS StartTimeRounded, DATEADD(HOUR, DATEDIFF(HOUR, 0, EndTime), 0) AS EndTimeRounded
+					FROM [$(MobiControlDB)].dbo.DeviceStatApplication AS A WITH (NOLOCK) 
+					INNER JOIN [$(MobiControlDB)].dbo.devInfo as D WITH (NOLOCK) ON A.DevId = D.DevId
+				WHERE A.Endtime <= @lastTime
 		END
 	END
 END
