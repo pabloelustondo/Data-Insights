1f1c53ca63c54e5a15876474fa61e25d5664dced
Doga Ister <doga.ister@soti.net>
2017-03-24 12:21:08 -0400

    CB-370: location of the busses update correctly

--- dad3/src/app/dad/map2.component.ts
+++ dad3/src/app/dad/map2.component.ts
@@ -18,36 +18,80 @@ import { DadMap } from './map.component';
     `,
 })
 export class DadMap2 implements OnInit {
+    _map: any;
+    markers: any[]; // Create a marker array to hold your markers
+    @Input()
 
-   @Input()
     map: DadChart;
     _data: any[];
     @Input()
     set data(d){
         if (d) {
             this._data = d.columns;
+            if (this._map){
+                let self = this;
+                // reset all markers first
+                for (let i =0; i < self.markers.length; i++) {
+                    self.markers[i].remove();
+                }
+                 this._data.forEach( function (x) {
+                     let busIcon = L.icon({
+                        iconUrl: '../../img/bus_green.png',
+                     });
+
+                     // this part draws them again
+                     let latLang: any = L.latLng(x[0], x[1]);
+                     let oneMarker:any = L.marker(latLang, {
+                         icon: busIcon
+                     });
+                     self.markers.push(oneMarker);
+
+                     oneMarker.addTo(self._map).openPopup();
+                 });
+            }
+          //  this.markerPoints.uptate();
+
         }
     };
 
     title: string = 'Next Bus Map 2';
 
     ngOnInit() {
-        let map = L.map('mapid').setView([this._data[0][0], this._data[0][1]], 13);
-        map.attributionControl.setPrefix(''); // Don't show the 'Powered by Leaflet' text.
+        let self = this;
+
+        this._map = L.map('mapid').setView([this._data[0][0], this._data[0][1]], 13);
+        //map.attributionControl.setPrefix(''); // Don't show the 'Powered by Leaflet' text.
 
         let tileUrl = 'https://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}.png?apikey=a96f5dd5205f4726a660af6e3f7c5c14',
             layer = new L.TileLayer(tileUrl, {maxZoom: 22});
 
 // add the layer to the map
-        map.addLayer(layer);
-
+        this._map.addLayer(layer);
+        this.markers = [];
         let busIcon = L.icon({
             iconUrl: '../../img/bus_green.png',
         });
 
         this._data.forEach( function (x) {
-            L.marker([x[0], x[1]], {icon: busIcon}).addTo(map)
-                .openPopup();
+
+            let latLang: any = L.latLng(x[0], x[1]); //create latLang for marker
+            // create marker
+            let oneMarker:any = L.marker(latLang, {
+               icon: busIcon
+            });
+
+            //store markers in array
+            self.markers.push(oneMarker);
+
+            //draw the point
+            oneMarker.addTo(self._map).openPopup();
+
+             /*L.marker([x[0], x[1]], {icon: busIcon}).addTo(self._map)
+                .openPopup();*/
+
+            //L.marker(latLang, {icon: busIcon}).addTo(self._map)
+            //    .openPopup();
+
         });
     }
 }
