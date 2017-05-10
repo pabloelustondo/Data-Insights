20288a344291c9f8a020d088f16274f8089a6785
Varun Dave <varun.dave@soti.net>
2017-05-10 09:06:55 -0400

    CB-443: provide sample tenants

--- dps/testing/spec/spec.js
+++ dps/testing/spec/spec.js
@@ -25,6 +25,7 @@ describe("Data Analytics Service - CDL", function() {
                                 tenantId: 'dpsTestTenantId',
                                 timeStamp: (new Date()).toISOString()
                             },
+                            clientMetadata : { },
                             clientData: [  {created: "Nov 3 1964", field1: "value1"},
                                      {created: "Nov 4 1964", field1: "value2"}
                                 ]
@@ -55,6 +56,7 @@ describe("Data Analytics Service - CDL", function() {
                                 tenantId: 'dpsTestTenantId2',
                                 timeStamp: (new Date()).toISOString()
                             },
+                            clientMetadata : { },
                             clientData:  {
                                 created: "Nov 3 1964",
                                 field1: "value1",
@@ -82,12 +84,95 @@ describe("Data Analytics Service - CDL", function() {
                     contentType:"application/json",
                     data: JSON.stringify(
                         {
+                            clientMetadata: {},
                             clientData:  {
                                 created: "Nov 3 1964",
-                                field1: "value1",
+                                fieldOne: "value1",
                                 nestedData: {
                                     created: "Nov 4 1964",
                                     field1: "value2"
+                                }
+                            }
+                        }
+                    ),
+                    success:function(data, textStatus, jqXHR) {
+                        var response = data.response;
+                        var testValue = 'Missing idaMetadata field';
+
+                        expect(data).toBe(undefined);
+                        expect(response).toBe(undefined);
+                        done();
+                    },
+                    error: function(request, textStatus, errorThrown) {
+                        expect(errorThrown).toBe('Bad Request');
+                        done();
+                    }
+                });
+        });
+
+
+        it("Makes a call to api without Client metadata " +
+            "it should get a 400 status and fail", function(done) {
+            $.ajax(
+                {   url:"/data/request",
+                    method:"POST",
+                    contentType:"application/json",
+                    data: JSON.stringify(
+
+                        {
+                            idaMetadata: {
+                                referer : 'sampleData',
+                                dataSourceId : '12345678901234567890',
+                                tenantId: 'varun_test',
+                                timeStamp: (new Date()).toISOString()
+                            },
+                            clientData:  {
+                                created: "Nov 3 1964",
+                                fieldOne: "value1",
+                                nestedData: {
+                                    created: "Nov 4 1964",
+                                    field1: "value2"
+                                }
+                            }
+                        }
+                    ),
+                    success:function(data, textStatus, jqXHR) {
+                        var response = data.response;
+                        var testValue = 'Missing clientMetadata field';
+
+                        expect(data).toBe(undefined);
+                        expect(response).toBe(undefined);
+                        done();
+                    },
+                    error: function(request, textStatus, errorThrown) {
+                        expect(errorThrown).toBe('Bad Request');
+                        done();
+                    }
+                });
+        });
+
+        it("testing if a user is able to find the tenant " +
+            "should work", function(done) {
+            $.ajax(
+                {   url:"/data/request",
+                    method:"POST",
+                    contentType:"application/json",
+                    data: JSON.stringify(
+                        {
+                            idaMetadata: {
+                                referer : 'sampleData',
+                                dataSourceId : 'uniqueId12345',
+                                tenantId: 'varun_test',
+                                timeStamp: (new Date()).toISOString()
+                            },
+                            clientMetadata : { },
+                            clientData:  {
+                                created: "Nov 3 1964",
+                                field1: "value1",
+                                field2: "value2",
+                                nestedData: {
+                                    created: "Nov 5 1964",
+                                    field1: "valuen"
                                 }
                             }
                         }
