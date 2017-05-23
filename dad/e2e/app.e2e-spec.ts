8f8468b3bff7f70dc02b1f4ac65bbd6702c05a5c
Doga Ister <doga.ister@soti.net>
2017-05-23 14:28:44 -0400

    CB-508: till it gets JWT token, protractor doesn't get it

--- dad/e2e/app.e2e-spec.ts
+++ dad/e2e/app.e2e-spec.ts
@@ -43,17 +43,37 @@ describe('should login to the page', () => {
     browser.get('http://localhost:3003');
   });
 
-  it("should ()put the user's tenant ID('test') and hit the login button", function() {
+  it("should put the user's tenant ID('test') and hit the login button", function() {
     let tenantID = element(by.id('domainid'));
     let button = element(by.className('btn btn-default'));
     tenantID.sendKeys('test');
-    sleep();
-    button.click()
+    browser.sleep(2000);
+    button.click();
     browser.ignoreSynchronization = true;
     browser.waitForAngular();
-    browser.sleep(3000);
     expect(browser.getCurrentUrl()).toEqual('http://localhost:3004/oauth/authorize?response_type=code&client_id=undefined&state=test');
-    sleep();
+  });
+
+  it("should put the user name('Doga') and hit the login button", function () {
+   //password can be empty
+    browser.get('http://localhost:3004/oauth/authorize?response_type=code&client_id=undefined&state=test');
+    let userName = element(by.id('username'));
+    let Password = element(by.id('password'));
+    let logBut = element(by.js ("return document.getElementsByName('login');"));
+    userName.sendKeys('Doga');
+    browser.sleep(2000);
+    Password.sendKeys('123456');
+    browser.sleep(2000);
+    logBut.click();
+    browser.sleep(1000);
+    expect(browser.getCurrentUrl()).toEqual("http://localhost:3003/#/home");
+  });
+
+  it("should come to the DAD", function (){
+    browser.get('http://localhost:4200/#/dad/page/batstats');
+    browser.sleep(2000);
+    expect(browser.getCurrentUrl).toEqual('http://localhost:4200/#/dad/page/batstats');
+    browser.sleep(2000);
   });
 
 });
