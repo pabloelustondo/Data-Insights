ba0fa7f4f1128d537db65db886b5489e5ec3f98c
Varun Dave <varun.dave@soti.net>
2017-05-30 09:24:10 -0400

    CB-494: proide placeholder to deal with kafka service

--- oda/src/controllers/topicsController.ts
+++ oda/src/controllers/topicsController.ts
@@ -9,6 +9,7 @@ import {SDS} from '../models/user';
 import {QueryModel} from '../models/queryModel';
 import {Metrics} from '../models/metrics';
 import * as express from '@types/express';
+import {KafkaService} from '../services/kafkaService';
 import * as rp from 'request-promise';
 // import * as https from 'https';
 const config = require('../../appconfig.json');
@@ -33,11 +34,25 @@ export class TopicsController {
         let req = request;
         let token = req.headers['x-access-token'];
 
+        let kafka = new KafkaService();
 
         if (token) {
 
             let mData = ['topics : string [] '];
 
+
+            let client = kafka.getClient('tenantId'); // pass in tenantId to get client for a tenant (R
+
+            /*
+            client.once('connect', function () {
+                client.loadMetadataForTopics([], function (error: any, results: any) {
+                    if (error) {
+                        return console.error(error);
+                    }
+                    console.log('%j', _.get(results, '1.metadata'));
+                });
+            });
+            */
             let testData = ['vehicleInfo', 'customTopic'];
             // returns test data for now
 
