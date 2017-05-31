b63da44f480d06b17fa9eab58cdcf6815d832c54
rgervais <ray.gervais@soti.net>
2017-05-31 10:01:56 -0400

    CB-518: Added Basic Angular Structure

---
+++ tmm/src/app/components/layout/UserInput/UserInput.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './UserInput.component.html',
+  styleUrls: ['./UserInput.component.css']
+})
+export class UserInput implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
