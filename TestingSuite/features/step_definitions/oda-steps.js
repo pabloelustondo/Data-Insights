6f6bfc7541a476071aa079834394afda0446328c
Shirley Xia <shirley.xia@soti.net>
2017-06-06 13:04:41 -0400

    cleaning up steps code

--- TestingSuite/features/step_definitions/oda-steps.js
+++ TestingSuite/features/step_definitions/oda-steps.js
@@ -6,7 +6,7 @@ var Cucumber = require('cucumber')
 // will work with all https requests will all libraries (i.e. request.js)
 
 Cucumber.defineSupportCode(function(context) {
-    var setWorldConstructor = context.setWorldConstructor;
+    const FS = require('fs');
     var Given = context.Given;
     var When = context.When;
     var Then = context.Then;
@@ -16,7 +16,7 @@ Cucumber.defineSupportCode(function(context) {
     var globalconfig = require(process.cwd()+'\\globalconfig_test.json');
     var accessToken='';
     var url = '';
-    const FS = require('fs');
+
     // Configure Client
     var options  = {
         "method": "",
@@ -35,8 +35,12 @@ Cucumber.defineSupportCode(function(context) {
     Given('I grab ODA port number from globalconfig.json', function (callback) {
         //I get ODA's port number from the url in config json file using REGEX
         var oda_url = globalconfig.oda_url;
-        if(oda_url == "" || oda_url == undefined) throw new Error('Cannot get port: ida url not in global config file');
+
+        if(oda_url == "" || oda_url == undefined)
+            throw new Error('Cannot get port: ida url not in global config file');
+
         var port_str = oda_url.match("[0-9]+")[0];
+
         if(isNaN(port_str)){
             throw new Error('Cannot get port: invalid global config file');
         }else{
@@ -52,16 +56,15 @@ Cucumber.defineSupportCode(function(context) {
             accessToken = contents;
             callback();
         });
-
     });
 
 
 
     Given('I set valid request header and body for POST call to ~/query', function (callback) {
         //prepare header and body for posting to IDA query endpoint
-        options.preambleCRLF = options.postambleCRLF = true;
         options.baseUrl = url +":" + odaPortNumber;
         options.headers['content-type'] = 'application/json';
+        //set example query in body
         options.body = {
             "dataSetId": "string",
             "from": [
@@ -82,7 +85,6 @@ Cucumber.defineSupportCode(function(context) {
     When('I GET topics', function (callback) {
         // Write code here that turns the phrase above into concrete actions
         resetOptions();
-        options.url = '/query/topics';
         options.baseUrl = url +":" + odaPortNumber;
         options.headers['content-type'] = 'application/json';
         options.headers['x-access-token'] = accessToken;
@@ -143,7 +145,7 @@ Cucumber.defineSupportCode(function(context) {
         // Write code here that turns the phrase above into concrete actions
         var resString = JSON.stringify(responseData).toLowerCase();
         if (resString.includes('query not supported') || !resString.includes('createdat'))
-            throw new Error("response message did not includes: " + stringInDoubleQuote);
+            throw new Error("error: " + resString);
         callback();
     });
 
