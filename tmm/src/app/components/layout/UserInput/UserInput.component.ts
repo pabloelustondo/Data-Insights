bcb6d15a8bb815431a2ccb1b678a8bb00584aad6
Doga Ister <doga.ister@soti.net>
2017-06-07 12:47:58 -0400

    CB-516: CRUD operations are done except delete

--- tmm/src/app/components/layout/UserInput/UserInput.component.ts
+++ tmm/src/app/components/layout/UserInput/UserInput.component.ts
@@ -3,34 +3,31 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import { DataSourceList } from './tmmModels';
 import { SmlTenantMetadata } from '../../../../sml/sml';
 
-import { CrudComponent } from './crud.component';
 import { SmlDataService } from '../../../../sml/data.service';
-import {smlTenantMetadataSample} from "./jsonEditorSchema.configuration";
+import {smlTenantMetadataSample, smlTenantMetadataEmpty} from "./jsonEditorSchema.configuration";
 
 
 @Component({
   selector: 'userinput' ,
   providers: [SmlDataService],
   template: `
-  <div>
-   <!--<crud [options]="DataSourceList"></crud>-->
-     
+  <div>     
     <div class="container">
       <br/>
-      <div>
-          <crud [options]='DataSourceList' (optionChanged)='optionChanged($event.target.value)'></crud> 
-      </div> <br/>
+      <button type="button" class="btn btn-primary" (click)="objectInit()">Click to add a Data set</button>
+      <br/><br/>
       <div class="row">
         <div class="col">
+          <h2>List of Your Data Sets</h2>
           <div class="list-group" *ngFor="let dataSet of tenantMetadata.dataSets">
-              <a class="list-group-item" (click)=editorOption(dataSet.id) [id]="dataSet.id">  {{ dataSet.name }}</a>
+              <a class="list-group-item" (click)=editorOption(dataSet.id) [id]="dataSet.id">{{ dataSet.name }}</a>
           </div>
         </div>
         
         <div class="col">
+          <h2>Editor</h2>
           <app-editor-smldatasource [dataSource] = "selectedOption" [(ngModel)]="currentItem" (optionUpdated)="optionUpdated($event)"></app-editor-smldatasource>
         </div>
-     
       </div>
     </div>
   </div>
@@ -41,6 +38,7 @@ export class UserInput implements OnInit {  //name will be sml tenant meta data 
   selectedOption: any;
   currentItem: any;
   tenantMetadata: any = smlTenantMetadataSample;
+  emptyDataSet: any = smlTenantMetadataEmpty;
 
   constructor() { }
 
@@ -61,4 +59,11 @@ export class UserInput implements OnInit {  //name will be sml tenant meta data 
     this.tenantMetadata.dataSets[updatedItem.index] = updatedItem;
     delete this.tenantMetadata.dataSets[updatedItem.index].index;
   }
+
+  objectInit() {
+    this.selectedOption = this.emptyDataSet;
+    this.selectedOption['index'] = this.tenantMetadata.dataSets.length;
+
+  }
+
 }
