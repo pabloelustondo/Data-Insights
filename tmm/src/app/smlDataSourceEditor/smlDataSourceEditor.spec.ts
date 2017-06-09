386ff1e7774ea24d626424ca77170eca906fe207
rgervais <ray.gervais@soti.net>
2017-06-09 16:58:46 -0400

    CB-527: Started Karma Tests for smlDataSourceEditor

--- tmm/src/app/smlDataSourceEditor/smlDataSourceEditor.spec.ts
+++ tmm/src/app/smlDataSourceEditor/smlDataSourceEditor.spec.ts
@@ -1,25 +1,43 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { EditorSMLDatasourceComponent } from './smlDataSourceEditor';
+import { smlDataSourceEditor } from './smlDataSourceEditor';
+import { JsonEditorComponent } from 'ng2-jsoneditor';
 
-describe('EditorSMLDatasourceComponent', () => {
-  let component: EditorSMLDatasourceComponent;
-  let fixture: ComponentFixture<EditorSMLDatasourceComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ EditorSMLDatasourceComponent ]
-    })
-    .compileComponents();
-  }));
+describe('The SML Data Source Editor Component', () => {
+  let component: smlDataSourceEditor;
+  let fixture: ComponentFixture<smlDataSourceEditor>;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(EditorSMLDatasourceComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    this.component = new smlDataSourceEditor();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should be created', () => {
+    expect(this.component).toBeTruthy();
+  });
+  it('should contain an empty dataSource', () => {
+    expect(this.component.dataSource == null).toBeTruthy();
+  });
+  it('should contain an empty json-editor object', () => {
+    expect(this.component.editor == null);
+  });
+  it('should contain a valid json-options object', () => {
+    expect(this.component.editorOptions).toBeTruthy();
+  });
+  it('the json-options should have "search" set to false', () => {
+    expect(this.component.editorOptions.search).toBeFalsy();
+  });
+  it('the json-options should include a array of "modes" which are text, tree, and view', () => {
+    let result = false;
+    this.component.editorOptions.modes.forEach(mode => {
+      result =  (mode === 'text' || mode === 'tree' || mode === 'view');
+    });
+    expect(result).toBeTruthy();
+  });
+  it('the saveCurrentItem function should contain a valid return object', () => {
+    this.component.dataSource = {
+      id: 'karmaTest',
+      name: 'karmaTest'
+    };
+    let savedObject = this.component.editor.get().stringify();
+    expect(savedObject === this.component.dataSource).toBeTruthy();
   });
 });
