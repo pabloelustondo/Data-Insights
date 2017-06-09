fd5cabd8038c3dd5b057b25401ec3e9bf7c26307
Varun Dave <varun.dave@soti.net>
2017-06-09 10:07:52 -0400

    CB-539: provide js files for dataLake and raw DataLake services

---
+++ dps/services/rawDataLakeService.js
@@ -0,0 +1,63 @@
+"use strict";
+/**
+ * Created by vdave on 5/8/2017.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+var rp = require("request-promise");
+var config = require('../config.json');
+var appconfig = require('../appconfig.json');
+var globalConfig = require('../globalconfig.json');
+var testResponses = require('../testing/testResponses.json');
+function uploadRawData(tenantId, dataSourceId, clientData) {
+    if (appconfig.testingmode) {
+        return new Promise(function (resolve) { resolve(testResponses.awsSampleResponse + tenantId); });
+    }
+    else {
+        var endpoint = globalConfig['cdl_url'] + '/transactionLog/' + tenantId + '/data';
+        var headerOptions = {
+            'x-access-token': config['access_token']
+        };
+        var body = {
+            tenantId: tenantId,
+            dataSourceId: dataSourceId,
+            clientData: clientData
+        };
+        var options = {
+            json: true,
+            method: 'POST',
+            headers: headerOptions,
+            url: endpoint,
+            body: body
+        };
+        return rp(options);
+    }
+}
+exports.uploadRawData = uploadRawData;
+function uploadModifiedData(tenantId, collectionName, clientData) {
+    if (appconfig.testingMode) {
+        return {
+            n: '1',
+            ok: '1'
+        };
+    }
+    else {
+        var endpoint = globalConfig['cdl_url'] + tenantId + config['cdl_put_endpoint'];
+        var headerOptions = {
+            'x-access-token': config['access_token']
+        };
+        var body = {
+            tenantId: tenantId,
+            dsId: collectionName,
+            data: clientData
+        };
+        var options = {
+            json: true,
+            method: 'POST',
+            headers: headerOptions,
+            url: endpoint,
+            body: body
+        };
+        return rp(options);
+    }
+}
+exports.uploadModifiedData = uploadModifiedData;
