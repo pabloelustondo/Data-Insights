97798746bb027a29b209984266aef310c89c7ef0
Ali <aliab@asu.edu>
2017-06-14 17:18:47 -0400

    deleting enrollments adedd. Tests implemented.

--- ddb/server.js
+++ ddb/server.js
@@ -223,8 +223,10 @@ router.post('/tenant/:tenantid', function(req,res){
 router.delete('/tenant/:tenantid', function(req,res){
     callDbAndRespond(req,res, function(req,res,db, next){
         db.collection('tenant').deleteMany({"tenantid":req.params.tenantid}, next);
+        db.collection('enrollments').deleteMany({"tenantid":req.params.tenantid}, next);
     });
 });
+
 
 ////////////////////////////
 // TENANT related APIS  //
