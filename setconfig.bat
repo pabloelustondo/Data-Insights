4cc0bde823e76510a1f5bfe4bf3304261a6aee0e
rgervais <ray.gervais@soti.net>
2017-06-21 10:15:47 -0400

    CB-568: Updated Deployment Files

--- setconfig.bat
+++ setconfig.bat
@@ -21,6 +21,9 @@ echo configfile %configfile%
 echo copying globalconfig for %1   %configfile%
 copy globalconfigs\%configfile% .\globalconfig.json
 
+
+echo "copying files to components"
 for %%x in ("cdl", "dad", "ddb", "dlm", "dos", "dps", "dss", "ida", "oda", "tmm") do (
     copy .\globalconfig.json .\%%x\globalconfig.json
-    )
+)
+
