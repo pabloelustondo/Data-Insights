1cceb4f3fb0c5c17b0e02538f26a9470b0073231
pabloelustondo <pablo.elustondo@gmail.com>
2017-06-26 14:06:41 -0400

    CB-437:

--- sml/pyh/compute_input.py
+++ sml/pyh/compute_input.py
@@ -4,22 +4,32 @@ import sys, json, numpy as np
 
 #Read data from stdin
 def read_in():
-    lines = sys.stdin.readlines()
-    #Since our input would only be having one line, parse our JSON data from that
-    return json.loads(lines[0])
+    lines = [1,2,3,4]
+    return json.loads(lines)
 
 def main():
+
+    print("Running Python")
+
+    print("This is the name of the script: ", sys.argv[0])
+    print("Number of arguments: ", len(sys.argv))
+    print("The arguments are: " , str(sys.argv))
+
     #get our data as an array from read_in()
-    lines = read_in()
+    lines = [1,2,3,4]
+
+    morelines = sys.stdin.read(2);
 
     #create a numpy array
+    print(lines)
+
     np_lines = np.array(lines)
 
     #use numpys sum method to find sum of all elements in the array
     lines_sum = np.sum(np_lines)
 
     #return the sum to the output stream
-    print lines_sum
+    print(lines_sum)
 
 #start process
 if __name__ == '__main__':
--- sml/pyh/start.js
+++ sml/pyh/start.js
@@ -1,6 +1,9 @@
 //start.js
 var spawn = require('child_process').spawn,
-    py    = spawn('python', ['compute_input.py']),
+    arg1 = "arg1",
+    arg2 = "arg2",
+    arg3 = "arg3",
+    py    = spawn('python', ['compute_input.py', arg1, arg2, arg3] ),
     data = [1,2,3,4,5,6,7,8,9],
     dataString = '';
 
