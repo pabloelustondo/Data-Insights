adc5b426414d3284c5d75b0b2b6199ab49d7b21c
Varun Dave <varun.dave@soti.net>
2017-06-27 11:38:54 -0400

    CB-584: provide a metadata controller

---
+++ ida/src/controllers/metadataController.ts
@@ -0,0 +1,176 @@
+/**
+ * Created by vdave on 6/27/2017.
+ */
+/**
+ * Created by vdave on 6/23/2017.
+ */
+/**
+ * Created by vdave on 5/2/2017.
+ */
+import {Route, Get, Example, Request, Response} from 'tsoa';
+import {SDS} from '../models/user';
+import {InputDataModel} from '../models/inputDataModel';
+import {ResponseModel, ErrorResponseModel} from '../models/responseModel';
+import {ListBatteryStats} from '../models/listBatteryStats';
+let jwt  = require('jsonwebtoken');
+import * as express from '@types/express';
+const path = require('path');
+const config = require('../../config.json');
+
+let kafka = require('kafka-node');
+import * as rp from 'request-promise';
+
+
+@Route('Metadata')
+export class MetadataController {
+
+    /**
+     * Post a unit of data to be stored in the cloud analytics database
+     */
+
+    @Response<ResponseModel>('200', 'OK')
+    @Response<ErrorResponseModel>('400', 'Missing Token')
+    @Response<ErrorResponseModel>('401', 'Token verification Failed')
+    @Response<ErrorResponseModel>('500', 'Internal Server Error. Please contact SOTI Support')
+    @Response<ErrorResponseModel>('400', 'Content-Type incorrect. Content-type must be Application/JSON')
+    @Response<ErrorResponseModel>('406', 'Wrong Input Model')
+    @Get('')
+    @Example<any>({
+        headers: {
+            'x-access-token': 'Temporary JWT that was retrieved from getAuthorizationToken endpoint',
+            'content-type': 'application/json'
+        },
+        json: true,
+        url: 'https://localhost:3010/metadata/:tenantId',
+
+    })
+    public async Create(tenantId: string, @Request() express: express.Request): Promise<ResponseModel> {
+
+
+        let req = express;
+        let ip = req.headers['x-forwarded-for'];
+        let token = req.headers['x-access-token'];
+        let contentType = req.headers['content-type'];
+
+
+        if (!token) {
+
+            return Promise.reject({
+                message: 'missing token',
+                status: '400'
+            });
+
+
+        } else {
+
+            if (contentType !== 'application/json') {
+                return Promise.reject({
+                    message: 'Content-Type incorrect. Content-type must be Application/JSON',
+                    status: '406'
+                });
+            }
+
+            let data = JSON.stringify(req.body);
+            try {
+                JSON.parse(data);
+            } catch (e) {
+                return Promise.reject({
+                    message: 'Content-Type incorrect. Body must be json type',
+                    status: '406'
+                });
+            }
+
+            let customerData: InputDataModel = express.body;
+            if (!((customerData.metadata) && (customerData.metadata.dataSetId) && (customerData.data))) {
+
+                return Promise.reject({
+                    message: 'Wrong input model',
+                    status: '406'
+                });
+            }
+
+            let verifyAndDecodeJwt = function () {
+                let promise = new Promise(function (resolve, reject) {
+                    try {
+                        resolve(jwt.verify(token, config['expiring-secret']));
+                    } catch (err) {
+                        console.log('could not verify token');
+                        reject( {
+                            message: err.message,
+                            statusCode: 401
+                        });
+                    }
+                });
+                return promise;
+            };
+
+
+            let sendToQueue = function (jwtDecodedToken: any) {
+
+                if (jwtDecodedToken) {
+                    let promise = new Promise(function (resolve, reject) {
+                        let xqs = {tenantId: tenantId};
+                        let server = require('../server');
+                        let appConfig = server.appconfig;
+                        const xurl = appConfig['dps_url'] + '/getMetadata/' + tenantId;
+
+                        const options: rp.OptionsWithUrl = {
+                            headers: {
+                                'x-api-key': config['aws-x-api-key']
+                            },
+                            json: true,
+                            method: 'GET',
+                            qs: xqs,
+                            url: xurl
+                        };
+
+                        resolve(rp(options));
+
+                    });
+                    return promise;
+
+                } else {
+                    return new Promise (function (resolve, reject) {
+                        reject({
+                            statusCode : 401,
+                            message : 'Invalid token'
+                        });
+                    });
+                }
+
+            };
+
+            let responseData = function (serverResonse: any) {
+                let promise = new Promise(function (resolve, reject) {
+                    if (serverResonse) {
+                        let mData = ['tenant metadata'];
+                        const user: any = {
+                            createdAt: new Date(),
+                            metadata: mData,
+                            data: 'OK'
+                        };
+                        resolve(user);
+                    } else {
+                        reject( {
+                            message : 'Error with backend Service',
+                            statusCode : 504
+                        });
+                    }
+                });
+                return promise;
+            };
+
+            let finalResponse: any = await verifyAndDecodeJwt().then(sendToQueue).then(responseData, function (err) {
+                console.log(err);
+
+                return Promise.reject(
+                    {
+                        message: err.message,
+                        status:  err.statusCode
+                    }
+                );
+            });
+            return finalResponse;
+        }
+    }
+}
