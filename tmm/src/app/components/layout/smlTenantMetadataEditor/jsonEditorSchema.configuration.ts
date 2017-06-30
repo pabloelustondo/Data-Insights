bcd3637a7b00666ee4371ee838ba037399533cd1
Jeffrey Hua <jeffrey.hua@soti.net>
2017-06-30 14:08:40 -0400

    CB-601: update sample json to work with new definition:

--- tmm/src/app/components/layout/smlTenantMetadataEditor/jsonEditorSchema.configuration.ts
+++ tmm/src/app/components/layout/smlTenantMetadataEditor/jsonEditorSchema.configuration.ts
@@ -6,75 +6,91 @@ import {SmlTenantMetadata, SmlDataSet} from '../../../../sml/sml';
 
 // this is the dataset schema that we need for JSON editor and crud component will use this too
 export const smlTenantMetadataSample: SmlTenantMetadata = {
-    id: 'SMLTest',  // id of the metadata
-    name: 'SML Data Set',
-    tenantId: 'testtenant-testuser',
-    dataSets: [{
-      id: '10-22-1',
-      name: 'Test Data Set 01',
-      from: ['Doga Ister'],
-      persist: true,
-      filter: ['/&AS/'],
-      merge: ['09-21-31$F'],
-      projections: [''],
-      metadata: ['']
-    }, {
-      id: '101',
-      name: 'Test Data Set 02',
-      from: ['Ray Gervais'],
-      persist: true,
-      filter: ['/&asdasdasda/'],
-      merge: ['09-1231-31$F'],
-      projections: [''],
-      metadata: ['']
-    }],
-    dataSources: [{
-      id: '10-23-1',
-      name: 'Test Data Set 01',
-      type: 'TestMockData',
-      active: true,
-      activationKey: '',
-      properties: ['Test', 'Doga', 'Is', 'AWesome'],
-      dataSets: ['']
-    }, {
-      id: '10-23-1',
-      name: 'Test Data Set 02',
-      type: 'TestMockData',
-      active: true,
-      activationKey: '',
-      properties: ['Test', 'Ray', 'Is', 'Bae'],
-      dataSets: ['']
-    }],
-    users: [{
-      id: '10-24-1',
-      name: 'Test Data Set 01',
-      permissions: ['Write', 'Read', 'Execute'],
-      status: 'Admin'
-    }, {
-      id: '32-1',
-      name: 'Test Data Set 02',
-      permissions: ['Write', 'Read'],
-      status: 'User'
-    }],
-    idpInformation: [{
-      id: '10-25-02',
-      name: 'idap Main Config',
-      endpoint: 'localhost:1023/endPointFTW',
-      configurations: [{
-        method: 'GET',
-        secure: 'x-access'
-      }]
+  id: 'SMLTest',  // id of the metadata
+  name: 'SML Data Set',
+  tenantId: 'testtenant-testuser',
+  dataSets: [{
+    id: '10-22-1',
+    name: 'Test Data Set 01',
+    from: ['Doga Ister'],
+    persist: true,
+    filter: ['/&AS/'],
+    merge: ['09-21-31$F'],
+    projections: [''],
+    metadata: ['']
+  }, {
+    id: '101',
+    name: 'Test Data Set 02',
+    from: ['Ray Gervais'],
+    persist: true,
+    filter: ['/&asdasdasda/'],
+    merge: ['09-1231-31$F'],
+    projections: [''],
+    metadata: ['']
+  }],
+  dataSources: [{
+    id: '10-23-1',
+    name: 'Test Data Set 01',
+    type: 'MCDP',
+    active: true,
+    activationKey: '',
+    properties: [
+      {
+        'inputName' : 'Url',
+        'inputValue' : 'http://cad109.soti.net/mc/api'
+      }],
+    dataSets: ['']
+  }, {
+    id: '10-23-1',
+    name: 'Test Data Set 02',
+    type: 'API',
+    active: true,
+    activationKey: '',
+    properties: [
+      {
+        'inputName' : 'Url',
+        'inputValue' : 'http://cad109.soti.net/mc/api'
       },
       {
-        id: '10-25-02',
-        name: 'idap Main Config 2',
-        endpoint: 'localhost:1023/endPointFTW',
-        configurations: [{
-          method: 'POST',
-          secure: 'y-access 4 pizza'
-        }]
+        'inputName' : 'Api Name',
+        'inputValue' : 'my custom api'
+      },
+      {
+        'inputName' : 'interval',
+        'inputValue' : '30'
+      }],
+    dataSets: ['']
+  }],
+  users: [{
+    id: '10-24-1',
+    name: 'Test Data Set 01',
+    permissions: ['Write', 'Read', 'Execute'],
+    status: 'Admin'
+  }, {
+    id: '32-1',
+    name: 'Test Data Set 02',
+    permissions: ['Write', 'Read'],
+    status: 'User'
+  }],
+  idpInformation: [{
+    id: '10-25-02',
+    name: 'idap Main Config',
+    endpoint: 'localhost:1023/endPointFTW',
+    configurations: [{
+      method: 'GET',
+      secure: 'x-access'
+    }]
+  },
+    {
+      id: '10-25-02',
+      name: 'idap Main Config 2',
+      endpoint: 'localhost:1023/endPointFTW',
+      configurations: [{
+        method: 'POST',
+        secure: 'y-access 4 pizza'
       }]
-  };
+    }]
+};
 
 export const smlTenantEmptyMetadataSample: SmlTenantMetadata =
   {
@@ -88,14 +104,14 @@ export const smlTenantEmptyMetadataSample: SmlTenantMetadata =
   };
 
 export const smlTenantMetadataEmpty: SmlDataSet = {
-    id: '',
-    name: '',
-    from: [''],
-    persist: true,
-    filter: '',
-    merge: '',
-    projections: '',
-    metadata: ['']
-  };
+  id: '',
+  name: '',
+  from: [''],
+  persist: true,
+  filter: '',
+  merge: '',
+  projections: '',
+  metadata: ['']
+};
 
 
