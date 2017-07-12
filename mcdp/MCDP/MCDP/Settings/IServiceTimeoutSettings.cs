2e71140d8319f8cb7c2fa94001ffa56c0551a5ca
Nathan Ma <nathan.ma@soti.net>
2017-07-12 16:43:32 -0400

    CB-619 Adjust Dataset

---
+++ mcdp/MCDP/MCDP/Settings/IServiceTimeoutSettings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Soti.MCDP.Settings
+{
+    /// <summary>
+    /// Service timeout settings
+    /// </summary>
+    public interface IServiceTimeoutSettings
+    {
+        /// <summary>
+        /// Gets the send timeout.
+        /// </summary>
+        TimeSpan SendTimeout { get; }
+
+        /// <summary>
+        /// Gets the receive timeout.
+        /// </summary>
+        TimeSpan ReceiveTimeout { get; }
+
+        /// <summary>
+        /// Gets the close timeout.
+        /// </summary>
+        TimeSpan СloseTimeout { get; }
+
+        /// <summary>
+        /// Gets the open timeout.
+        /// </summary>
+        TimeSpan OpenTimeout { get; }
+    }
+}
---
+++ mcdp/MCDP/Metadata/Model/DeviceSyncStatus.cs
@@ -0,0 +1,45 @@
+﻿namespace Soti.MCDP.Metadata.Model
+{
+    /// <summary>
+    /// Used in the get DeviceSyncStatus
+    /// </summary>
+    public class DeviceSyncStatus
+    {
+        public string Name { get; set; }
+
+        public Status Status { get; set; }
+
+        public string LastSyncTime { get; set; }
+
+        public string PreviousSyncTime { get; set; }
+
+        public DeviceSyncStatus() {}
+
+        public DeviceSyncStatus(string name, Status status, string lastSyncTime, string previousSyncTime)
+        {
+            this.Name = name;
+            this.Status = status;
+            this.LastSyncTime = lastSyncTime;
+            this.PreviousSyncTime = previousSyncTime;
+        }
+    }
+    
+    public enum Status
+    {
+        /// <summary>
+        /// Message indicates a fail to send data
+        /// </summary>
+        Failed = -1,
+        /// <summary>
+        /// Message indicates in progress on current table
+        /// </summary>
+        Inprogress = 1,
+
+        /// <summary>
+        /// Message indicates a data send success
+        /// </summary>
+        Pass = 2
+
+ 
+    }
+}
